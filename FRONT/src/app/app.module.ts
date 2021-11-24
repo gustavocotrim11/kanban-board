567d19d483fcabab944a26d5f6cdedc5b0a6068c
Gustavo Cotrim <gustavocotrim11@gmail.com>
2021-11-24 10:36:22 -0300

    Create app

---
+++ FRONT/src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './pages/login/login.component';
+import { BoardComponent } from './pages/board/board.component';
+
+@NgModule({
+  declarations: [AppComponent, LoginComponent, BoardComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
